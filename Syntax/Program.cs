a4fad4c7928af985f5c0260f0db4eb5fa84443d0
Casey Hudler <chudler624@gmail.com>
2022-08-11 15:08:46 -0400

    initial commit

---
+++ Syntax/Program.cs
@@ -0,0 +1,53 @@
+﻿//// original given
+//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+
+//Console.WriteLine(response);
+
+//inferred
+
+//var answer = 4;
+//var response = "";
+
+//if (answer < 9)
+//{
+//  response = answer + " is less than nine";
+//}
+//else
+//{
+//  response = answer + " is greater than or equal to nine";
+//}
+
+//Console.WriteLine(response);
+
+
+
+//string interpolation
+
+int answer = 4;
+
+if (answer < 9)
+{
+    Console.WriteLine($"{answer} is less than nine");
+}
+else
+{
+    Console.WriteLine($"{answer} is greater than or equal to nine");
+}
+
+
+
+//ternary operator
+
+int answer = 4;
+string response;
+response = (answer < 9)? ($"{answer} is less than 9") : ($"{answer} is greater than 9");
+Console.WriteLine(response);
